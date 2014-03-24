14e452c51f279a93b99e83e8fdf2773f12620de8
Andy Edmonds <andy@edmonds.be>
2014-03-24 14:45:01 +0100

    updates... not a rickroll

--- mcn/sm/so_manager.py
+++ mcn/sm/so_manager.py
@@ -81,55 +81,54 @@ class SOManager():
                 SLA == gold, size of gear should be large
         '''
 
-        create_app_headers['X-OCCI-Attribute'] = 'occi.app.name=' + entity.identifier
+        #TODO - check sting, ALPHANUM only
+        # re.match('[a-zA-Z0-9_]', MYSTR)
+        create_app_headers['X-OCCI-Attribute'] = 'occi.app.name=' + 'CHANGEME'
         self.conn.request('POST', '/app/', headers=create_app_headers)
         resp = self.conn.getresponse()
         #TODO error handling
-        app_uri_frag = resp.getheader('Location')
+        from urlparse import urlparse
+        app_uri_path = urlparse(resp.getheader('Location')).path
 
         # get git uri
-        self.conn.request('GET', app_uri_frag, headers={'Accept: text/occi'})
+        self.conn.request('GET', app_uri_path, headers={'Accept': 'text/occi'})
         resp = self.conn.getresponse()
         attrs = resp.getheader('X-OCCI-Attribute')
         repo_uri = ''
         for attr in attrs.split(', '):
-            if attr.find('occi.app.url') != -1:
+            if attr.find('occi.app.repo') != -1:
                 repo_uri = attr.split('=')[1]
-            else:
-                print('Oooops - no occi.app.url found!')
+                break
 
-        return app_uri_frag, repo_uri
+        return app_uri_path, repo_uri
 
     def deploy_app(self, repo):
         """
-        Deploy the local SO bundle
-        assumption here
-        - a git repo is returned
-        - the bundle is not managed by git
+            Deploy the local SO bundle
+            assumption here
+            - a git repo is returned
+            - the bundle is not managed by git
         """
 
-        #XXX assumes that git is installed
-        if repo.startswith('git'):
-            # create temp dir...and clone the remote repo provided by OpS
-            dir = tempfile.mkdtemp()
-            os.system(' '.join(['git', 'clone', repo, dir]))
+        # XXX assumes that git is installed AND public key is registered
+        # create temp dir...and clone the remote repo provided by OpS
+        dir = tempfile.mkdtemp()
+        # XXX this will not work unless the SMs public key is available to the NBAPI
+        os.system(' '.join(['git', 'clone', repo, dir]))
 
-                # Get the SO bundle
-            bundle_loc = CONFIG.get('service_manager', 'bundle_location')
-            shutil.copytree(bundle_loc, dir)
+        # Get the SO bundle
+        bundle_loc = CONFIG.get('service_manager', 'bundle_location')
+        shutil.copytree(bundle_loc, dir)
 
-            # put OpenShift stuff in place
-            # build and pre_start_python comes from 'support' directory in bundle
-            # TODO this needs to be improved
-            shutil.copyfile(bundle_loc+'/build', os.path.join(dir, '.openshift', 'action_hooks', 'build'))
-            shutil.copyfile(bundle_loc+'/pre_start_python', os.path.join(dir, '.openshift', 'action_hooks', 'pre_start_python'))
+        # put OpenShift stuff in place
+        # build and pre_start_python comes from 'support' directory in bundle
+        # TODO this needs to be improved - could use from mako.template import Template?
+        shutil.copyfile(bundle_loc+'/support/build', os.path.join(dir, '.openshift', 'action_hooks', 'build'))
+        shutil.copyfile(bundle_loc+'/support/pre_start_python', os.path.join(dir, '.openshift', 'action_hooks', 'pre_start_python'))
 
-            os.system(' '.join(['chmod', '+x', os.path.join(dir, '.openshift', 'action_hooks', '*')]))
+        os.system(' '.join(['chmod', '+x', os.path.join(dir, '.openshift', 'action_hooks', '*')]))
 
-            # add & push to OpenShift
-            os.system(' '.join(['cd', dir, '&&', 'git', 'add', '-A']))
-            os.system(' '.join(['cd', dir, '&&', 'git', 'commit', '-m', '"deployment of SO for tenant X"', '-a']))
-            os.system(' '.join(['cd', dir, '&&', 'git', 'push']))
-
-        else:
-            print('oopppssss - I do not know how to deploy here!')
+        # add & push to OpenShift
+        os.system(' '.join(['cd', dir, '&&', 'git', 'add', '-A']))
+        os.system(' '.join(['cd', dir, '&&', 'git', 'commit', '-m', '"deployment of SO for tenant X"', '-a']))
+        os.system(' '.join(['cd', dir, '&&', 'git', 'push']))
