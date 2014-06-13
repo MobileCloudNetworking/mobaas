3a8d5bdca73ee43c5ba905feefae4d4571f68a67
Andy Edmonds <andy@edmonds.be>
2014-06-13 19:40:26 +0200

    adds default value functionality to ConfigParser, adds performance timing flag to command line

--- mcn/sm/__init__.py
+++ mcn/sm/__init__.py
@@ -23,46 +23,14 @@ import time
 
 DOING_PERFORMANCE_ANALYSIS = True
 
-def config_logger(log_level=logging.DEBUG):
-    logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S %p',
-                        log_level=log_level)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(log_level)
-    return logger
+class DefaultConfigParser(ConfigParser.ConfigParser):
+    def get(self, section, option, default='', raw=False, vars=None):
+        try:
+            value = ConfigParser.ConfigParser.get(self, section, option, raw, vars)
+        except ConfigParser.NoOptionError:
+            value = default
+        return value
 
-LOG = config_logger()
-
-parser = OptionParser(usage="Usage: %prog options. See %prog -h for options.")
-
-parser.add_option("-c", "--config",
-                  action="store",
-                  type="string",
-                  dest="config_file_path",
-                  help="Path to the service manager configuration file.")
-
-(options, args) = parser.parse_args()
-
-if not options.config_file_path:
-    parser.error("Wrong number of arguments.")
-
-LOG.info('Using config file: ' + options.config_file_path)
-
-CONFIG = ConfigParser.ConfigParser()
-CONFIG.read(options.config_file_path)
-
-# helper function for to measure timedelta.
-def timeit(method):
-
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-
-        LOG.debug("%r (%r, %r) %2.2f sec" % (method.__name__, args, kw, te-ts))
-        return result, te-ts
-
-    return timed
 
 class conditional_decorator(object):
     def __init__(self, dec, condition):
@@ -74,3 +42,53 @@ class conditional_decorator(object):
             return func
         else:
             return self.decorator(func)
+
+
+def timeit(method):
+    # helper function for to measure timedelta.
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        LOG.debug("%r (%r, %r) %2.2f sec" % (method.__name__, args, kw, te-ts))
+        return result, te-ts
+
+    return timed
+
+def config_logger(log_level=logging.DEBUG):
+    logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        log_level=log_level)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+    return logger
+
+def get_config_file():
+    parser = OptionParser(usage="Usage: %prog options. See %prog -h for options.")
+    parser.add_option("-c", "--config-file",
+                      action="store",
+                      type="string",
+                      dest="config_file_path",
+                      help="Path to the service manager configuration file.")
+    parser.add_option("-p", "--perf-timings",
+                      action="store_true",
+                      dest="perf_timings",
+                      default=False,
+                      help="Enable performance timings within the SM.")
+    (options, args) = parser.parse_args()
+
+    if not options.config_file_path:
+        parser.error("Wrong number of arguments.")
+
+    return options
+
+options = get_config_file()
+
+DOING_PERFORMANCE_ANALYSIS = options.perf_timings
+
+LOG = config_logger()
+LOG.info('Using configuration file: ' + options.config_file_path)
+
+CONFIG = DefaultConfigParser()
+CONFIG.read(options.config_file_path)
